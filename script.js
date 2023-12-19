dd3b644b33848384e8a2777ce6c6e21800dcc18b
Kaustav Ray <140186859+kaustavRay1@users.noreply.github.com>
2023-12-19 17:29:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const cardsContainer = document.querySelector(".container");
 
 cardsContainer.addEventListener("click", (e) => {
-  const target = e.target.closest(".card");
+  const target = e.target.closest(".card1");
 
   if (!target) return;
 
-  cardsContainer.querySelectorAll(".card").forEach((card) => {
-    card.classList.remove("active");
+  cardsContainer.querySelectorAll(".card").forEach((card1) => {
+    card1.classList.remove("active");
   });
 
   target.classList.add("active");
